11d39b72f47bea8b02d259e90a1cf25b5b309c34
Glitch (zgleman-stock) <none>
2019-10-23 09:47:28 +0000

    🍳🐈 Checkpoint ./tests/1_unit-tests.js:7900686/100 ./routes/api.js:7900686/472 ./package.json:7900686/46

--- routes/api.js
+++ routes/api.js
@@ -10,9 +10,15 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+const mongoose=require('mongoose');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
+mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true});
+var db = mongoose.connection;
+db.once('open', function(){console.log('Connected to Database')});
+const Stock = mongoose.model('Stock', {stock: String,
+                                      price: String,
+                                      likes: Number});
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -7,9 +7,7 @@
 */
 
 var chai = require('chai');
-var StockHandler = require('../controllers/stockHandler.js');
 
-var stockPrices = new StockHandler();
 
 suite('Unit Tests', function(){
 
