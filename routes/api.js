1fee9837897c4249dbb4da2b7fa6da7f9ba83781
Glitch (zgleman-stock) <none>
2019-10-23 12:00:26 +0000

    💐⛷ Checkpoint ./routes/api.js:7900686/1339

--- routes/api.js
+++ routes/api.js
@@ -34,41 +34,20 @@ module.exports = function (app) {
         if (count > 0) {
           Stock.findOne({stock: stockName}, function(err, data){
             if (err) return console.log('error in findOne');
-            data.price = https.get("https://repeated-alpaca.glitch.me/v1/stock/" + stockName.toUpperCase() + "/quote", (resp) => {
-              let data = '';
-
-              resp.on('data', (chunk) => {
-                data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+            let url = "https://repeated-alpaca.glitch.me/v1/stock/" + stockName.toLowerCase() + "/quote"
+            data.price = https.get(url, function(resp){
+                resp.on('data', function(chunk){
+                  return  chunk.latestPrice;
+                });
+                }).on("error", function(e){
+                    console.log("Got error: " + e.message);
+            });;
             req.query.like == true ? data.likes++ : null;
             data.save().then(
             res.json({stockData: {stock: data.stock, price: data.price, likes: data.likes}}))
           })
         } else if (count == 0) {
-          var price = https.get("https://repeated-alpaca.glitch.me/v1/stock/" + stockName.toUpperCase() + "/quote", (resp) => {
-              let data = '';
-
-              resp.on('data', (chunk) => {
-                data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+          var price = '';
           var likes = 0;
           req.query.like == true ? likes++ : null;
           Stock.create({stock: stockName,
