00c3b5581958343849f08fc4d8e0d3da81203cdc
Glitch (zgleman-stock) <none>
2019-10-24 11:55:55 +0000

    👜😇 Checkpoint ./getStocks.js:7900686/1647 ./controller/getStock:7900686/1647 ./routes/api.js:7900686/5324

--- routes/api.js
+++ routes/api.js
@@ -17,14 +17,51 @@ const CONNECTION_STRING = process.env.DB;
 mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.once('open', function(){console.log('Connected to Database')});
-
+const Stock = mongoose.model('Stock', {stock: String,
+                                      price: String,
+                                      likes: Number});
 module.exports = function (app) {
   
   app.route('/api/stock-prices')
     .get(function (req, res){
+      function getStock(stockName, like){
+        Stock.countDocuments({ stock: stockName }, async function(err, count){
+        if (err) return console.log('error in count');
+        if (count > 0) {
+          Stock.findOne({stock: stockName}, async function(err, data){
+            if (err) return console.log('error in findOne');
+            let url = 'https://repeated-alpaca.glitch.me/v1/stock/' + stockName + '/quote';
+            let response = await fetch(url);
+            let raw = await response.json();
+            data.price = raw.latestPrice;
+            like == true ? data.likes++ : null;
+            data.save().then(function(data){
+            let obj = {stockData: {stock: data.stock, price: data.price, likes: data.likes}};
+            return obj;})
+            
+          })
+        } else if (count == 0) {
+          let url = 'https://repeated-alpaca.glitch.me/v1/stock/' + stockName + '/quote';
+          let response = await fetch(url);
+          let raw = await response.json();
+          var price = raw.latestPrice;
+          var likes = 0;
+          like == true ? likes++ : null;
+          Stock.create({stock: stockName,
+                       price: price,
+                       likes: likes}, function(err, data){
+            if (err) return console.log('error saving doc');
+            let obj = {stockData: {stock: data.stock, price: data.price, likes: data.likes}};
+            return obj;
+          })
+        }
+      })
+  };
+    
+    
       var stockName = req.query.stock;
       var like = req.query.like;
-      
+      console.log(stockName);
     
     if(Array.isArray(stockName)){
       
