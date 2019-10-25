c29d4efac3845d080cf5b445cc235b41bef17c6d
Glitch (zgleman-stock) <none>
2019-10-25 07:08:42 +0000

    🏡🍚 Checkpoint ./routes/api.js:7900686/2292

--- routes/api.js
+++ routes/api.js
@@ -40,8 +40,55 @@ module.exports = function(app) {
       let url2 = "https://repeated-alpaca.glitch.me/v1/stock/" + stockName[1] + "/quote";
       let response2 = await fetch(url2);
       let raw2 = await response2.json();
-      
-      res.json({ wait: "route in progress" });
+      Stock.countDocuments({ stock: stockName[0] }, async function(err, count) {
+        if (err) return console.log("error in count");
+        if (count > 0) {
+          Stock.findOne({ stock: stockName[0] }, async function(err, data) {
+            if (err) return console.log("error in findOne");
+            data.price = raw1.latestPrice;
+            like == true ? data.likes++ : null;
+            data.save().then(function(data) {
+              Stock.countDocuments({ stock: stockName[1] }, async function(err, count) {
+        if (err) return console.log("error in count");
+        if (count > 0) {
+          Stock.findOne({ stock: stockName[1] }, async function(err, data2) {
+            if (err) return console.log("error in findOne");
+            data2.price = raw2.latestPrice;
+            like == true ? data2.likes++ : null;
+            data2.save().then(function(data2) {
+              res.json({stockData: {stock: data.stock, price: data.price, likes: data.likes}});              
+            });
+          });
+        } else if (count == 0) {
+          var price = raw2.latestPrice;
+          var likes = 0;
+          like == true ? likes++ : null;
+          Stock.create(
+            { stock: stockName[1], price: price, likes: likes },
+            function(err, data2) {
+              if (err) return console.log("error saving doc");
+              res.json({ stockData: [{ stock: data.stock, price: data.price, rel_likes: data.likes},{ stock: data2.stock, price: data.price, rel_likes: data.likes}});
+            }
+          );
+        }
+      });
+                          
+            });
+          });
+        } else if (count == 0) {
+          var price = raw1.latestPrice;
+          var likes = 0;
+          like == true ? likes++ : null;
+          Stock.create(
+            { stock: stockName[0], price: price, likes: likes },
+            function(err, data) {
+              if (err) return console.log("error saving doc");
+              res.json({ stockData: { stock: data.stock, price: data.price, likes: data.likes}});
+            }
+          );
+        }
+      });
+     
     } else {
             let url = "https://repeated-alpaca.glitch.me/v1/stock/" + stockName + "/quote";
             let response = await fetch(url);
