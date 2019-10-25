b1c111c33a2a47db97e09f97644af0172993be76
Glitch (zgleman-stock) <none>
2019-10-25 07:19:47 +0000

    🏊🏼🎉 Checkpoint ./routes/api.js:7900686/22118

--- routes/api.js
+++ routes/api.js
@@ -25,19 +25,20 @@ const Stock = mongoose.model("Stock", {
   likes: Number
 });
 
- 
 module.exports = function(app) {
   app.route("/api/stock-prices").get(async function(req, res) {
     var stockName = req.query.stock;
     var like = req.query.like;
-    
+
     console.log(stockName);
 
     if (Array.isArray(stockName)) {
-      let url1 = "https://repeated-alpaca.glitch.me/v1/stock/" + stockName[0] + "/quote";
+      let url1 =
+        "https://repeated-alpaca.glitch.me/v1/stock/" + stockName[0] + "/quote";
       let response1 = await fetch(url1);
       let raw1 = await response1.json();
-      let url2 = "https://repeated-alpaca.glitch.me/v1/stock/" + stockName[1] + "/quote";
+      let url2 =
+        "https://repeated-alpaca.glitch.me/v1/stock/" + stockName[1] + "/quote";
       let response2 = await fetch(url2);
       let raw2 = await response2.json();
       Stock.countDocuments({ stock: stockName[0] }, async function(err, count) {
@@ -49,30 +50,29 @@ module.exports = function(app) {
             like == true ? data.likes++ : null;
             data.save().then(function(data) {
               Stock.countDocuments({ stock: stockName[1] }, async function(err, count) {
-        if (err) return console.log("error in count");
-        if (count > 0) {
-          Stock.findOne({ stock: stockName[1] }, async function(err, data2) {
-            if (err) return console.log("error in findOne");
-            data2.price = raw2.latestPrice;
-            like == true ? data2.likes++ : null;
-            data2.save().then(function(data2) {
-              res.json({stockData: {stock: data.stock, price: data.price, likes: data.likes}});              
-            });
-          });
-        } else if (count == 0) {
-          var price = raw2.latestPrice;
-          var likes = 0;
-          like == true ? likes++ : null;
-          Stock.create(
-            { stock: stockName[1], price: price, likes: likes },
-            function(err, data2) {
-              if (err) return console.log("error saving doc");
-              res.json({ stockData: [{ stock: data.stock, price: data.price, rel_likes: data.likes},{ stock: data2.stock, price: data.price, rel_likes: data.likes}});
-            }
-          );
-        }
-      });
-                          
+                if (err) return console.log("error in count");
+                if (count > 0) {
+                  Stock.findOne({ stock: stockName[1] }, async function(err, data2) {
+                    if (err) return console.log("error in findOne");
+                    data2.price = raw2.latestPrice;
+                    like == true ? data2.likes++ : null;
+                    data2.save().then(function(data2) {
+                      res.json({stockData: [{ stock: data.stock, price: data.price, rel_likes: data.likes }, { stock: data2.stock, price: data2.price, rel_likes: data2.likes}]});
+                    });
+                  });
+                } else if (count == 0) {
+                  var price = raw2.latestPrice;
+                  var likes = 0;
+                  like == true ? likes++ : null;
+                  Stock.create(
+                    { stock: stockName[1], price: price, likes: likes },
+                    function(err, data2) {
+                      if (err) return console.log("error saving doc");
+                      res.json({stockData: [{ stock: data.stock, price: data.price, rel_likes: data.likes }, { stock: data2.stock, price: data2.price, rel_likes: data2.likes}]});
+                    }
+                  );
+                }
+              });
             });
           });
         } else if (count == 0) {
@@ -83,17 +83,40 @@ module.exports = function(app) {
             { stock: stockName[0], price: price, likes: likes },
             function(err, data) {
               if (err) return console.log("error saving doc");
-              res.json({ stockData: { stock: data.stock, price: data.price, likes: data.likes}});
+              Stock.countDocuments({ stock: stockName[1] }, async function(err, count) {
+                if (err) return console.log("error in count");
+                if (count > 0) {
+                  Stock.findOne({ stock: stockName[1] }, async function(err, data2) {
+                    if (err) return console.log("error in findOne");
+                    data2.price = raw2.latestPrice;
+                    like == true ? data2.likes++ : null;
+                    data2.save().then(function(data2) {
+                      res.json({stockData: [{ stock: data.stock, price: data.price, rel_likes: data.likes }, { stock: data2.stock, price: data2.price, rel_likes: data2.likes}]});
+                    });
+                  });
+                } else if (count == 0) {
+                  var price = raw2.latestPrice;
+                  var likes = 0;
+                  like == true ? likes++ : null;
+                  Stock.create(
+                    { stock: stockName[1], price: price, likes: likes },
+                    function(err, data2) {
+                      if (err) return console.log("error saving doc");
+                      res.json({stockData: [{ stock: data.stock, price: data.price, rel_likes: data.likes }, { stock: data2.stock, price: data2.price, rel_likes: data2.likes}]});
+                    }
+                  );
+                }
+              });
             }
           );
         }
       });
-     
     } else {
-            let url = "https://repeated-alpaca.glitch.me/v1/stock/" + stockName + "/quote";
-            let response = await fetch(url);
-            let raw = await response.json();
-        Stock.countDocuments({ stock: stockName }, async function(err, count) {
+      let url =
+        "https://repeated-alpaca.glitch.me/v1/stock/" + stockName + "/quote";
+      let response = await fetch(url);
+      let raw = await response.json();
+      Stock.countDocuments({ stock: stockName }, async function(err, count) {
         if (err) return console.log("error in count");
         if (count > 0) {
           Stock.findOne({ stock: stockName }, async function(err, data) {
@@ -101,7 +124,13 @@ module.exports = function(app) {
             data.price = raw.latestPrice;
             like == true ? data.likes++ : null;
             data.save().then(function(data) {
-              res.json({stockData: {stock: data.stock, price: data.price, likes: data.likes}});              
+              res.json({
+                stockData: {
+                  stock: data.stock,
+                  price: data.price,
+                  likes: data.likes
+                }
+              });
             });
           });
         } else if (count == 0) {
@@ -112,7 +141,13 @@ module.exports = function(app) {
             { stock: stockName, price: price, likes: likes },
             function(err, data) {
               if (err) return console.log("error saving doc");
-              res.json({ stockData: { stock: data.stock, price: data.price, likes: data.likes}});
+              res.json({
+                stockData: {
+                  stock: data.stock,
+                  price: data.price,
+                  likes: data.likes
+                }
+              });
             }
           );
         }
